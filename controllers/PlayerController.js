35bbfc7d9957370d346ac3c7ef9d57990aabf08f
KaisarFS <kaisar.10rpl2.2019@gmail.com>
2023-01-10 15:16:31 +0700

    [SAR] Add: getPlayers, getPlayerProfile

--- controllers/PlayerController.js
+++ controllers/PlayerController.js
@@ -4,12 +4,28 @@ class PlayerController {
     static async getPlayers(req, res, next) {
         try {
             const dataPlayer = await Player.findAll({
-                
+                attributes: { exclude: ['createdAt', 'updatedAt'] }
             })
 
-            console.log(dataPlayer);
+            res.status(200).json(dataPlayer)
+
         } catch (error) {
             console.log(error);
+            next(error)
+        }
+    }
+    
+    static async getPlayerProfile(req, res, next) {
+        try {
+            const {id} = req.params
+
+            const findPlayer = await Player.findByPk(id)
+            if(!findPlayer) throw {name: 'Player not found'}
+
+            res.status(200).json(findPlayer)
+
+        } catch (error) {
+            console.log(error, '<----');
             next(error)
         }
     }
--- routes/playerRoutes.js
+++ routes/playerRoutes.js
@@ -2,6 +2,7 @@ const PlayerController = require('../controllers/PlayerController')
 const router = require('express').Router()
 
 router.get('/', PlayerController.getPlayers)
+router.get('/:id', PlayerController.getPlayerProfile)
 
 
 module.exports = router
