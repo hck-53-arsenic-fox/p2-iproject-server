7455ada944aec87bd9993f0c204404632051266a
raihanqowi <raihanqowiliansu@gmail.com>
2023-01-10 20:59:39 +0700

    add-Feature-Register

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -1,7 +1,16 @@
 const {User} = require('../models')
 
 class Controller {
-
+static async handleRegister(req,res,next){
+    try {
+        const {email, password, name} = req.body
+        const dataUser = await User.create({email, password, name})
+        res.status(201).json({id:dataUser.id, email: dataUser.email})
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
 
 
 }
