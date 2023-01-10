2d597fed92a3e1ed81799c8476acb6e57802dc85
flamousz <valflamous@gmail.com>
2023-01-11 03:17:04 +0700

    post, delete feature in cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -20,6 +20,52 @@ class CartController {
             next(err)
         }
     }
+
+    static async postCart(req, res, next) {
+        try {
+            const UserId = req.user.id;
+            const {amount} = req.body
+            const { ProductId } = req.params;
+
+            const product = await Product.findByPk(ProductId);
+            if (!product) {
+                 throw {
+                      name: "DataShowProductNotFound",
+                 };
+            }
+
+            await Cart.create({
+                UserId,
+                ProductId,
+                amount
+           });
+
+           res.status(201).json(`${amount} of ${product.name} added to your cart`);
+
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    static async deleteCart(req, res, next) {
+        try {
+            const {id} = req.params
+
+            const cart = await Cart.findByPk(id)
+            if (!cart) {
+                throw { name: 'Cartnotfound' }
+            }
+
+            await Cart.destroy({
+                where: {id}
+            })
+            res.status(200).json('product successfully deleted from your cart')
+
+        } catch (err) {
+            next(err)
+        }
+    }
+
 }
 
 
