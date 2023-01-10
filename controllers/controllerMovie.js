5498d497a788706dd1b6f2b791a4d8b8afa33a37
rayhanathallahariq <rayhanariq59@gmail.com>
2023-01-10 18:02:28 +0700

    pagination

--- controllers/controllerMovie.js
+++ controllers/controllerMovie.js
@@ -2,8 +2,30 @@ const { Favorite, Movie, User } = require('../models')
 
 class controllerMovie{
     static async read(req, response,next) {
+        const { page } = req.query;
+        const paramQuerySQL = {};
+        let limit;
+        let offset;
+
+        // pagination
+        if (page !== '' && typeof page !== 'undefined') {
+        if (page.size !== '' && typeof page.size !== 'undefined') {
+            limit = page.size;
+            paramQuerySQL.limit = limit;
+        }
+
+        if (page.number !== '' && typeof page.number !== 'undefined') {
+            offset = page.number * limit - limit;
+            paramQuerySQL.offset = offset;
+        }
+        } else {
+        limit = 20; 
+        offset = 0;
+        paramQuerySQL.limit = limit;
+        paramQuerySQL.offset = offset;
+        }
         try {
-            let data = await Movie.findAll()
+            let data = await Movie.findAll(paramQuerySQL)
             response.status(200).json(data)
         } catch(error) {
             response.status(500).json({message:'Internal Server Error'})
