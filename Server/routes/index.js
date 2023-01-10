82fa32cf95aeb0e462cf34f19e5a111cf33d9a64
Hardy Yuantheo <hyuantheo@gmail.com>
2023-01-10 14:36:39 +0700

    fetch-data/details

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
-const genshin = require('genshin-api')
+
+// GENSHIN API
+const genshin = require('genshin-api')  // BAD
+const { EnkaClient, AssetsNotFoundError } = require('enka-network-api')
+const enka = new EnkaClient()
+
 
 const {User, Favorite} = require('../models')
 const {encryptPass, decryptPass} = require('../helper/bcrypt')
@@ -48,8 +53,14 @@ router.post('/twitter-login', async function(req, res, next){
 
 router.get('/characters', async function(req, res, next){
     try {
-        let data = await genshin.Characters({id: 1})
+        const characters = enka.getAllCharacters()
+        // console.log(characters.map(c => c.skills));
+        
+        let data = characters.map(c => {
+            return {id: c.id, name: c.name.get("en"), icon: c.icon.url}
+        })
         res.status(200).json(data)
+
     } catch (err) {
         next()
     }
@@ -57,8 +68,24 @@ router.get('/characters', async function(req, res, next){
 
 router.get('/characters/:id', async function(req, res, next){
     try {
+        const {id} = req.params
+        const oneChara = enka.getCharacterById(+id)
+
+        let passiveSkills = (oneChara.passiveTalents.map(ele => {
+            return {name: ele.name.get("en"), icon: ele.icon.url, description: ele.description.get("en")}
+        }));
         
+        let activeSkills = (oneChara.skills.map(ele => {
+            return {icon: ele.icon.url, description: ele.description.get("en")}
+        }));
+
+        let detail = ({id: oneChara.id, name: oneChara.name.get("en"), image: oneChara.splashImage.url, talents: passiveSkills, skills: activeSkills})
+        res.status(200).json(detail)
     } catch (err) {
+        if(err.name === 'AssetsNotFoundError'){
+            err.name = 'DataNotFound'
+            next(err)
+        }
         next(err)
     }
 })
