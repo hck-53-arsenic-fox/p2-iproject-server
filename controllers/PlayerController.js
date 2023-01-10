d378dbbeeea53aec198e2754757ab353cfa4daa5
KaisarFS <kaisar.10rpl2.2019@gmail.com>
2023-01-11 02:28:28 +0700

    Add: get all favorites data by username

--- controllers/PlayerController.js
+++ controllers/PlayerController.js
@@ -3,6 +3,9 @@ const { User, Player, Favorite } = require('../models')
 class PlayerController {
     static async getPlayers(req, res, next) {
         try {
+            const { access_token } = req.headers
+            if (!access_token) throw { name: 'Invalid token' }
+
             const dataPlayer = await Player.findAll({
                 attributes: { exclude: ['createdAt', 'updatedAt'] }
             })
@@ -14,13 +17,13 @@ class PlayerController {
             next(error)
         }
     }
-    
+
     static async getPlayerProfile(req, res, next) {
         try {
-            const {id} = req.params
+            const { id } = req.params
 
             const findPlayer = await Player.findByPk(id)
-            if(!findPlayer) throw {name: 'Player not found'}
+            if (!findPlayer) throw { name: 'Player not found' }
 
             res.status(200).json(findPlayer)
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -26,6 +26,7 @@ router.post('/login', UserController.login)
 
 router.use(authentication)
 
+router.get('/following', UserController.getAllFavorites)
 router.get('/:username', UserController.getUserProfile)
 router.patch('/:username', UserController.changeStatusPro)
 router.post('/:PlayerId', UserController.addFavoritePlayer)
