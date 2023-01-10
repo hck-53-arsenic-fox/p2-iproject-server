010e0c328171f56d32f33aef0559e468548159dd
flamousz <valflamous@gmail.com>
2023-01-10 21:24:14 +0700

    login google

---
+++ controllers/userController.js
@@ -0,0 +1,40 @@
+const { hashPassword } = require('../helpers/bcrypt')
+const { verify } = require('../helpers/google-login')
+const { createToken } = require('../helpers/jwt')
+const { User } = require('../models/index')
+
+
+
+class UserController {
+    
+    static async loginGoogle(req, res, next) {
+        try {
+            let {google_token} = req.headers
+            let password = hashPassword('passwordgoogle')
+            const googleEmail = await verify(google_token)
+            const [user, created] = await User.findOrCreate({
+                where: { email: googleEmail },
+                default: {
+                    email: googleEmail,
+                    password,
+                    username: 'Google User'
+                },
+                hooks: false
+            })
+            let payload = { id: user.id }
+            let access_token = createToken(payload)
+            res.status(200).json({
+                access_token,
+                email: user.email
+            })
+            
+
+        } catch (err) {
+            next(err)
+        }
+    }
+
+}
+
+
+module.exports = UserController
