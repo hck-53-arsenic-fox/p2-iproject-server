b3480b7896b51ce3efbbfc7f7e23b565f12087cb
Patrick_Andrada <patrickanthonio993@gmail.com>
2023-01-11 14:59:22 +0700

    do_username

--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const router = express.Router()
-
+const{authentication} = require("../middleware/authentication")
 
 const UserController = require("../controllers/userController")
+
 router.post("/users/register", UserController.register)
 router.post("/users/login", UserController.login)
+router.use(authentication)
+router.get("/users/username", UserController.userName)
 
 module.exports = router
