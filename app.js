1b04281dcdbf012ede2c598f3394058bc7f6ded6
Faldi0126 <muhrifaldi26@gmail.com>
2023-01-11 11:39:38 +0700

    Completed login and register

--- app.js
+++ app.js
@@ -29,9 +29,24 @@ app.use(cors());
 //? Register
 app.post('/register', async (req, res, next) => {
   try {
+    if (!req.body.email) throw { name: "Email is required" }
+    if (!req.body.password) throw { name: "Password is required" }
+    if (!req.body.username) throw { name: "Username is required" }
 
+    let input = {
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 10),
+      username: req.body.username
+    }
+
+    let user = await User.create(input)
+    res.status(201).json({
+      id: user.id,
+      email: user.email,
+      username: user.username
+    })
   } catch (error) {
-
+    next(error)
   }
 })
 
@@ -39,9 +54,24 @@ app.post('/register', async (req, res, next) => {
 //? Login
 app.post('/login', async (req, res, next) => {
   try {
+    if (!req.body.email) throw { name: "Email is required" }
+    if (!req.body.password) throw { name: "Password is required" }
 
+    let user = await User.findOne({
+      where: { email: req.body.email }
+    })
+
+    if (!user) throw { name: "Invalid email or password" }
+
+    let isMatch = bcrypt.compareSync(req.body.password, user.password)
+
+    if (!isMatch) throw { name: "Invalid email or password" }
+
+    let access_token = jwt.sign({ id: user.id }, process.env.JWT_SECRET)
+
+    res.status(200).json({ access_token })
   } catch (error) {
-
+    next(error)
   }
 })
 
@@ -54,12 +84,37 @@ app.get('/resorts', async (req, res, next) => {
     )
     res.status(200).json(data)
   } catch (error) {
-    console.log(error);
+    next(error)
   }
 })
 
+//? Modular Authentication
+const authentication = async (req, res, next) => {
+  try {
+    let { access_token } = req.headers
+    console.log(req.headers);
+    console.log(access_token);
+    if (!access_token) throw { name: "Invalid token" }
+    console.log(access_token);
+
+    let payload = jwt.verify(access_token, process.env.JWT_SECRET)
+
+    let user = await User.findByPk(payload.id)
+
+    req.user = { id: user.id }
+
+    console.log(user);
+    if (!user) throw { name: "Invalid token" }
+    next()
+  } catch (error) {
+    console.log(error);
+    next(error)
+  }
+}
+
+
 //? Create Resorts
-app.post('/resorts', upload.single('image'), async (req, res, next) => {
+app.post('/resorts', upload.single('image'), authentication, async (req, res, next) => {
   try {
     const geoData = await geocoder.forwardGeocode({
       query: req.body.location,
@@ -74,13 +129,12 @@ app.post('/resorts', upload.single('image'), async (req, res, next) => {
       location: req.body.location,
       geometry: geoData.body.features[0].geometry,
       imageUrl: req.body.imageUrl,
-      UserId: req.body.UserId
+      UserId: req.user.id
     }
     let data = await Resort.create(input)
-
+    res.status(201).json(data)
   } catch (error) {
-    console.log(req.body);
-    console.log(error);
+    next(error)
   }
 })
 
@@ -88,15 +142,17 @@ app.post('/resorts', upload.single('image'), async (req, res, next) => {
 app.get('/resorts/:id', async (req, res, next) => {
   try {
     let data = await Resort.findByPk(req.params.id, { include: [User] })
+    if (!data) throw { name: "Data not found" }
+
     res.status(200).json(data)
   } catch (error) {
-    console.log(error);
+    next(error)
   }
 })
 
 
 
-//! Uthentication
+//! Authorization
 app.use(async (req, res, next) => {
   try {
 
@@ -104,10 +160,6 @@ app.use(async (req, res, next) => {
 
   }
 })
-
-
-
-
 
 
 
@@ -128,8 +180,30 @@ app.use(async (req, res, next) => {
 
 //! Error Handler
 app.use((err, req, res, next) => {
+  console.log(err);
   let statusCode = 500
   let message = 'Internal Server Error'
+
+  if (err.name === 'SequelizeValidationError' || err.name === 'SequelizeUniqueConstraintError') {
+    statusCode = 400
+    message = err.errors[0].message
+  }
+  else if (err.name === 'Email is required' || err.name === "Password is required" || err.name === "Username is required") {
+    statusCode = 400
+    message = err.name
+  }
+  else if (err.name === 'Invalid email or password') {
+    statusCode = 401
+    message = err.name
+  }
+  else if (err.name === 'Data not found') {
+    statusCode = 404
+    message = err.name
+  }
+  else if (err.name === 'Invalid token') {
+    statusCode = 401
+    message = err.name
+  }
 
 
 
