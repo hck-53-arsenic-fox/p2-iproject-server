bc0b555703a377a431b7817183226ca3032d451e
qonit-filza <109884831+qonit-filza@users.noreply.github.com>
2023-01-11 10:52:29 +0700

    seed countries data

---
+++ migrations/20230111034309-create-country.js
@@ -0,0 +1,30 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Countries', {
+      countryCode: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        primaryKey: true,
+      },
+      countryName: {
+        type: Sequelize.STRING,
+      },
+      currencyCode: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Countries');
+  },
+};
