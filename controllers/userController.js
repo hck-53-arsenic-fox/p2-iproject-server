67250761118f9e2120d745d4ae78fdaad7f8901e
Patrick_Andrada <patrickanthonio993@gmail.com>
2023-01-11 15:09:17 +0700

    subscription

--- controllers/userController.js
+++ controllers/userController.js
@@ -116,6 +116,29 @@ class UserController {
     }
   }
   // ******** Midtrans Payment Closed ************* //
+
+  // ********** Subscription Open ********** //
+  static async subscription(req, res, next) {
+    try {
+      let data = await User.update(
+        {
+          isSubscribed: true,
+        },
+        {
+          where: {
+            id: req.user.id,
+          },
+        }
+      );
+      res.status(201).json({
+        message: `User with id ${req.user.id} has become a Premium member`,
+      });
+    } catch (error) {
+      console.log(error, "<<<< dari patch");
+      next(error);
+    }
+  }
+  // ********** Subscription Closed ******** //
 }
 
 module.exports = UserController;
