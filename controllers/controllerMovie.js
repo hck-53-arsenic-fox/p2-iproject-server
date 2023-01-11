dff26ef6556622fc496151cf9df248c980689bd5
rayhanathallahariq <rayhanariq59@gmail.com>
2023-01-11 10:07:56 +0700

    midtrans server + install midtrans

--- controllers/controllerMovie.js
+++ controllers/controllerMovie.js
@@ -1,4 +1,5 @@
 const { Favorite, Movie, User } = require('../models')
+const midtransClient = require('midtrans-client');
 
 class controllerMovie{
     static async read(req, response,next) {
@@ -111,6 +112,47 @@ class controllerMovie{
             response.status(200).json(data)
         } catch(error) {
             response.status(500).json({message:'Internal Server Error'})
+        }
+    }
+    static async payment(req,response){
+        try {
+            await User.update({status:'Premium'},{where:{id:req.user.id}})
+            response.status(200).json({message:`Your status Premium Now`})
+        } catch (error) {
+            response.status(500).json({message:'Internal Server Error'})
+        }
+    }
+    static async genMidToken(req,response){
+        try {
+            const findUser = await User.findByPk(req.user.id)
+            if(findUser.status==='Premium'){
+                throw{name:'Already_Premium'}
+            }
+            let snap = new midtransClient.Snap({
+            // Set to true if you want Production Environment (accept real transaction).
+            isProduction : false,
+            serverKey : process.env.MIDTRANS_SERVER_KEY
+            });
+
+            let parameter = {
+                transaction_details: {
+                    order_id: "TRANSACTION_"+Math.floor(1000000+Math.random()*9000000),
+                    gross_amount: 10000
+                },
+                credit_card:{
+                    secure : true
+                },
+                customer_details: {
+                    email: findUser.email
+                }
+            };
+
+        const midtransToken = await snap.createTransaction(parameter) 
+        response.status(201).json(midtransToken)
+        } catch (error) {
+            if(error.name==='Already_Premium'){
+                response.status(400).json({message:'Already Premium'})
+            }
         }
     }
 }
--- routers/movie.js
+++ routers/movie.js
@@ -5,6 +5,8 @@ const { authentication, authorization } = require('../middlewares/middleware')
 
 router.get('/allmovie',authentication,controllerMovie.read)
 router.get('/favorite/allfavorite',authentication,controllerMovie.allFav)
+router.patch('/payment',authentication,controllerMovie.payment)
+router.post('/genmidtoken',authentication,controllerMovie.genMidToken)
 router.get('/detail/:id',authentication,authorization,controllerMovie.readById)
 router.post('/favorite/:id',authentication,controllerMovie.favorite)
 router.delete('/favorite/:id',authentication,controllerMovie.deleteFav)
