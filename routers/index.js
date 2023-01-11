61197ac2ee04d24ec0811edb4fbf74734b8a4b1a
Patrick_Andrada <patrickanthonio993@gmail.com>
2023-01-11 15:06:25 +0700

    do_midtrans

--- routers/index.js
+++ routers/index.js
@@ -9,4 +9,5 @@ router.post("/users/login", UserController.login)
 router.use(authentication)
 router.get("/users/username", UserController.userName)
 router.get("/users/profile", UserController.profile)
+router.post("/users/paymentToken", UserController.midtrans)
 module.exports = router
