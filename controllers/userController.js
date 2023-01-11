3e4aad326fb2a016242b1ac449c62cee70d01ad0
Patrick_Andrada <patrickanthonio993@gmail.com>
2023-01-11 14:57:21 +0700

    do_login

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,7 +28,32 @@ class UserController {
   }
   // ******* Register Closed ********* //
 
-  
+  // ******* Login Open ********* //
+  static async login(req, res, next) {
+    const { email, password } = req.body;
+    try {
+      if (!email) throw { status: 401, message: "Email is required" };
+      if (!password) throw { status: 401, message: "Password is required" };
+      let user = await User.findOne({ where: { email: email } });
+      if (!user) throw { status: 400, message: "Invalid email/password" };
+      let compared = comparehash(password, user.password);
+      if (!compared) throw { status: 400, message: "Invalid email/password" };
+      let payload = { id: user.id };
+      let access_token = createToken(payload);
+      res.status(200).json({ access_token: access_token });
+    } catch (error) {
+      if (error.message === "Email is required") {
+        error = error.message;
+      } else if (error.message === "Password is required") {
+        error = error.message;
+      } else if (error.message === "Invalid email/password") {
+        error = "Invalid email/password";
+      }
+      res.status(400).json({ error });
+      // next(error)
+    }
+  }
+  // ******* Login Closed ********* //
 }
 
 module.exports = UserController;
--- routers/index.js
+++ routers/index.js
@@ -4,6 +4,6 @@ const router = express.Router()
 
 const UserController = require("../controllers/userController")
 router.post("/users/register", UserController.register)
-
+router.post("/users/login", UserController.login)
 
 module.exports = router
