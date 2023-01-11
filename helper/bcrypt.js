1578f7b3419c266a03d65d16d74f2e620f298d6e
Patrick_Andrada <patrickanthonio993@gmail.com>
2023-01-11 18:08:46 +0700

    doingLogin

--- helper/bcrypt.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-let hashPassword = (password) => bcrypt.hashSync(password);
-let comparehash = (password, hashedPassword) =>
-  bcrypt.compareSync(password, hashedPassword);
-
-module.exports = { hashPassword, comparehash };
