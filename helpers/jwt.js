e1a8f7c78a901f0ef2424b1a23e138fab6acf314
akbarridho <akbarrizqullah029@gmail.com>
2023-01-12 11:26:17 +0700

    Success deploy

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 const signToken = (payload) => {
-    let token = jwt.sign(payload, process.env.SECRET);
+    let token = jwt.sign(payload, process.env.SECRET_KEY);
     return token
 }
 
 const decodedToken = (token) => {
-    let decoded = jwt.verify(token, process.env.SECRET);
+    let decoded = jwt.verify(token, process.env.SECRET_KEY);
     return decoded
 }
 
