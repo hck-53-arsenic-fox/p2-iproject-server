8ee71e9ba355ec3c5a145629415cf509e2fe568c
fahrisshohib <fahrisshohib@gmail.com>
2023-01-12 11:56:49 +0700

    fix port on railway

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 const express = require("express");
 const app = express();
-const port = 4444 || process.env.PORT;
+const port = process.env.PORT || 4444;
 const cors = require("cors");
 const ControllerUser = require("./controllers/controllerUser");
 const router = require("./routes/index");
@@ -12,12 +12,8 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/public", express.static("./src"));
-
 app.use(router);
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/public", express.static("./src"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
