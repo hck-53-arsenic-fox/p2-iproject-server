f9247f19303de2ef048b13a6ac24a481bd90aac2
rezadulhaq <dhiaulhaqreza@gmail.com>
2023-01-12 12:15:36 +0700

    change port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const CustomerController = require("./controllers/customerController");
 const router = require("./routes");
 const handleError = require("./middlewares/errorHandler");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use(express.json());
